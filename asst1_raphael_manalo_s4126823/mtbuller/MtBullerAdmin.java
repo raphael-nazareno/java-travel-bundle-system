282b948ce4a654c61464188141f3657aeeee7616
raphael-nazareno <raphaelnazareno.mb@gmail.com>
2026-04-08 13:38:12 +1000

    created arraylist for customers in Mt Buller resort class - added an addCustomer function and create an instance of MtBullerResort in MtBullerAdmin

--- asst1_raphael_manalo_s4126823/mtbuller/MtBullerAdmin.java
+++ asst1_raphael_manalo_s4126823/mtbuller/MtBullerAdmin.java
@@ -4,6 +4,8 @@ public class MtBullerAdmin {
 
     public static void main(String[] args) {
 
+        MtBullerResort resort = new MtBullerResort();
+
         Customer alice = new Customer(00001, "Alice Piper", 23, "Female",
                 SkiingLevel.BEGINNER);
 
--- asst1_raphael_manalo_s4126823/mtbuller/MtBullerResort.java
+++ asst1_raphael_manalo_s4126823/mtbuller/MtBullerResort.java
@@ -1,4 +1,16 @@
 package mtbuller;
 
+import java.util.ArrayList;
+
 public class MtBullerResort {
+    private ArrayList<Customer> customers;
+
+    public MtBullerResort() {
+        customers = new ArrayList<>();
+    }
+
+    public void addCustomer(Customer c) {
+        customers.add(c);
+    }
+
 }
