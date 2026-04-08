b6f6ec534b3ac4b6e842e64f7902b7d6b7e92417
raphael-nazareno <raphaelnazareno.mb@gmail.com>
2026-04-08 15:36:42 +1000

    Add getters and availability setter to Accommodation class

--- asst1_raphael_manalo_s4126823/mtbuller/Accommodation.java
+++ asst1_raphael_manalo_s4126823/mtbuller/Accommodation.java
@@ -5,4 +5,26 @@ public abstract class Accommodation {
     private double pricePerNight;
     private boolean available;
 
+    public Accommodation(int id, double pricePerNight) {
+        this.id = id;
+        this.pricePerNight = pricePerNight;
+        this.available = true;
+    }
+
+    public int getID() {
+        return this.id;
+    }
+
+    public double getPricePerNight() {
+        return this.pricePerNight;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public boolean isAvailable() {
+        return this.available;
+    }
+
 }
