be5c9e28cee64365e71403951037b5c739b45d89
raphael-nazareno <raphaelnazareno.mb@gmail.com>
2026-04-08 22:35:03 +1000

    Implement lift pass pricing logic with discount and season pricing

--- asst1_raphael_manalo_s4126823/mtbuller/LiftPass.java
+++ asst1_raphael_manalo_s4126823/mtbuller/LiftPass.java
@@ -6,4 +6,15 @@ public class LiftPass {
     public LiftPass(int numberOfDays) {
         this.numberOfDays = numberOfDays;
     }
+
+    public double calculatePrice() {
+        if (numberOfDays == 30) {
+            return 200.0;
+        }
+        if (numberOfDays >= 5) {
+            return (26 * numberOfDays) * .9;
+        } else {
+            return 26 * numberOfDays;
+        }
+    }
 }
