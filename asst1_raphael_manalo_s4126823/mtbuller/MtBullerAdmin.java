1b176ea6a866d6d2ac424c2d39f8c0a63bfc1161
raphael-nazareno <raphaelnazareno.mb@gmail.com>
2026-04-08 14:58:12 +1000

    added third customer as per assignment requirements

--- asst1_raphael_manalo_s4126823/mtbuller/MtBullerAdmin.java
+++ asst1_raphael_manalo_s4126823/mtbuller/MtBullerAdmin.java
@@ -7,13 +7,17 @@ public class MtBullerAdmin {
         MtBullerResort resort = new MtBullerResort();
 
         Customer alice = new Customer("Alice Piper", 23, "Female",
-                SkiingLevel.BEGINNER);
+                SkiingLevel.EXPERT);
 
         Customer grace = new Customer("Grace Parker", 23, "Female",
                 SkiingLevel.INTERMEDIATE);
 
+        Customer mark = new Customer("Mark Howard", 35, "Male",
+                SkiingLevel.BEGINNER);
+
         resort.addCustomer(alice);
         resort.addCustomer(grace);
+        resort.addCustomer(mark);
 
         resort.listCustomers();
     }
