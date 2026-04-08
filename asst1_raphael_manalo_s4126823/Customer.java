d92ae3ef3bccb086b15bdf8e60b92e9910b88fb9
s4126823 <s4126823@student.rmit.edu.au>
2026-04-08 12:10:35 +1000

    moved customer  objects to main class and created default toString message

--- asst1_raphael_manalo_s4126823/Customer.java
+++ asst1_raphael_manalo_s4126823/Customer.java
@@ -20,7 +20,7 @@ public class Customer {
 
     }
 
-    Customer Alice = new Customer(00001, "Alice Piper", "25th of September, 2002", 23, "Female");
-
-    Customer Grace = new Customer(00002, "Grace Parker", "7th of December, 2002", 23, "Female");
+    public String toString() {
+        return String.format("This is %s, with customerID: %d", customerName, customerID);
+    }
 }
---
+++ asst1_raphael_manalo_s4126823/MtBullerAdmin.java
@@ -0,0 +1,12 @@
+public class MtBullerAdmin {
+
+    public static void main(String[] args) {
+
+        Customer Alice = new Customer(00001, "Alice Piper", "25th of September, 2002", 23, "Female");
+
+        Customer Grace = new Customer(00002, "Grace Parker", "7th of December, 2002", 23, "Female");
+
+        System.out.println(Alice);
+        System.out.println(Grace);
+    }
+}
