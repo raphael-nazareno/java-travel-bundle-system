d15324ee76eb0199d4ad8f38c538f9cf06bba5ed
raphael-nazareno <raphaelnazareno.mb@gmail.com>
2026-04-08 16:00:08 +1000

    Add Apartment subclass with constructor and toString

--- asst1_raphael_manalo_s4126823/mtbuller/Apartment.java
+++ asst1_raphael_manalo_s4126823/mtbuller/Apartment.java
@@ -1,5 +1,12 @@
 package mtbuller;
 
 public class Apartment extends Accommodation {
+    public Apartment(int id, double pricePerNight) {
+        super(id, pricePerNight);
+    }
 
+    @Override
+    public String toString() {
+        return "Apartment | " + super.toString();
+    }
 }
