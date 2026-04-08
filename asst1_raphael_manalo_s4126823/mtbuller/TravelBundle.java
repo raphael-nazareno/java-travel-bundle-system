5dc7fc1238ef608fda1535c6aea96cb621c669e3
raphael-nazareno <raphaelnazareno.mb@gmail.com>
2026-04-08 16:51:55 +1000

    Add core fields to TravelBundle class

--- asst1_raphael_manalo_s4126823/mtbuller/TravelBundle.java
+++ asst1_raphael_manalo_s4126823/mtbuller/TravelBundle.java
@@ -1,5 +1,12 @@
 package mtbuller;
 
 public class TravelBundle {
+    private int bundleID;
+    private static int nextID = 1;
 
+    private String startDate;
+    private int durationDays;
+
+    private Customer customer;
+    private Accommodation accommodation;
 }
