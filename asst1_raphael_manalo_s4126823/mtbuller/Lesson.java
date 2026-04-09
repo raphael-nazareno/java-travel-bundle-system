6cd137c769b63099a5913e65ff89a76fd9616465
s4126823 <s4126823@student.rmit.edu.au>
2026-04-09 18:05:04 +1000

    Implement lesson pricing based on skiing level

--- asst1_raphael_manalo_s4126823/mtbuller/Lesson.java
+++ asst1_raphael_manalo_s4126823/mtbuller/Lesson.java
@@ -7,4 +7,13 @@ public class Lesson {
         this.level = level;
     }
 
+    public double calculatePrice() {
+        if (level == SkiingLevel.BEGINNER) {
+            return 25;
+        }
+        if (level == SkiingLevel.INTERMEDIATE) {
+            return 20;
+        }
+        return 15;
+    }
 }
