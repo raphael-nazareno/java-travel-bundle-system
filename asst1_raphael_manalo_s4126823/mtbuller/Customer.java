53ccbf0f6c8bbb2763ad8535f956b2bc43aca019
raphael-nazareno <raphaelnazareno.mb@gmail.com>
2026-04-10 00:58:22 +1000

    Make Customer class serializable for bundle persistence

--- asst1_raphael_manalo_s4126823/mtbuller/Customer.java
+++ asst1_raphael_manalo_s4126823/mtbuller/Customer.java
@@ -1,6 +1,9 @@
 package mtbuller;
 
-public class Customer {
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private int customerID;
     private static int nextID = 1;
